04a79137fd15cafe22884e931ff22259219fef31
TEJ-1-8 <127389564+TEJ-1-8@users.noreply.github.com>
2023-03-09 20:09:24 +0530

    Table of a Number
    JAVA Program to read a number and print its multiplication table upto 10.

---
+++ TableOfANumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class TableOfANumber 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int n= sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n+"*"+i+"="+n*i);
+		}
+		sc.close();
+	}
+}
