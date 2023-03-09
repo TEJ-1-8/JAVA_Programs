d540badd79de6bed7e0da4a353e79a84fe43b688
TEJ-1-8 <127389564+TEJ-1-8@users.noreply.github.com>
2023-03-09 15:47:28 +0530

    Natural Numbers
    JAVA Program that reads a number and prints that many Natural numbers.

---
+++ Natural.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Natural
+{
+  public static void main(String[] args) 
+	{
+    Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+			System.out.println(i+1);
+ 	}
+}
